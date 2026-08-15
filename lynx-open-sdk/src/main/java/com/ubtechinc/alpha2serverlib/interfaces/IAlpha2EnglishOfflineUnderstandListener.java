ce5596b88f29f2554ce65786087078d0aa41dc02
arthurson <110549393+arthurson@users.noreply.github.com>
2026-08-16 03:11:47 +0800

    Delete lynx-open-sdk directory

--- lynx-open-sdk/src/main/java/com/ubtechinc/alpha2serverlib/interfaces/IAlpha2EnglishOfflineUnderstandListener.java
+++
@@ -1,6 +0,0 @@
-package com.ubtechinc.alpha2serverlib.interfaces;
-
-/** Result of an offline English semantic-understanding request. */
-public interface IAlpha2EnglishOfflineUnderstandListener {
-   void onAlpha2EnglishOfflineUnderStandResult(String result);
-}
